0ffd291b0ca7639188e462e050a3b9fb6461e4b3
LaureBre <acs.laure.eole@gmail.com>
2018-04-20 15:11:45 +0200

    Add <img> creation function in Object

--- src/objects.js
+++ src/objects.js
@@ -1,26 +1,36 @@
 $( function() {			// initialisation JQuery
 
-
+	var scene = $('#scene');
   	var inventory = [];
 
 	var Element = {
-		init: function (name, urlImage, movable, combinable, zoomable, bagable, clue, message, inputable) {
+		init: function (name, urlImage, posLeft, posTop, width, draggable, combinable, zoomable, clue, message, inputable) {
 			this.name = name;
 			this.urlImage = urlImage;
 			this.dom = $('#'+name);
-			this.movable = movable;
+			this.draggable = draggable;
 			this.combinable = combinable;
 			this.zoomable = zoomable;
-			this.bagable = bagable;
 			this.inputable = inputable;
 			this.clue = clue;
 			this.message = message;
 			this.width = this.dom.width();
 			this.height = this.dom.height();
-			this.inBag();
-			// this.left = this.dom.offset().left;
-			// this.top = this.dom.offset().top;
+			this.dom.css('left', posLeft + "px");
+			this.dom.css('top', posTop + "px");
+			this.dom.width(width + "px");
 
+			this.createHTML();
+
+			this.inBag();
+		},
+
+		createHTML: function() {
+			var CSSclass = "element " 	+ (this.draggable ? "draggable " : '') 
+										+ (this.combinable ? "combinable " : '') 
+										+ (this.zoomable ? "zoomable " : '')
+										+ (this.inputable ? "inputable" : '');
+			scene.append('<img src="public/img/' + this.urlImage + '" alt="' + this.name + '" id="' + this.name + '" class="' + CSSclass + '">');
 		},
 
 		inBag: function () {
@@ -54,7 +64,7 @@ $( function() {			// initialisation JQuery
 
 		shiftPlace: function() {
 			var thisdom = this.dom;
-				if (this.movable) {
+				if (this.draggable) {
 
 					if (this.isInInventory == true) {
 						thisdom.inScene();
@@ -105,91 +115,91 @@ $( function() {			// initialisation JQuery
         }
 
 	};
-	
+
 
 	// Objets de la 1ère pièce
 	var apple = Object.create(Element);
-	apple.init('apple', 'img/apple.svg', false, false, false, false, "", "Ceci est une pomme", false);
+	apple.init('apple', 'apple.svg', 500, 0, 150, false, false, false,  "", "Ceci est une pomme", false);
 	
 	var banana = Object.create(Element);
-	banana.init('banana', 'img/banana.svg', false, false, false, false, "", "Ceci est une banane", false);
+	banana.init('banana', 'banana.svg', 500, 0, 150, false, false, false,  "", "Ceci est une banane", false);
 		
 	var battery = Object.create(Element);
-	battery.init('battery', 'img/battery.svg', true, true, false, true, "", "A quoi ces piles pourraient-elles bien me servir?", false);
+	battery.init('battery', 'battery.svg', 500, 0, 150, true, true, false,  "", "A quoi ces piles pourraient-elles bien me servir?", false);
 
 	var blocked = Object.create(Element);
-	blocked.init('battery', 'img/blocked.svg', false, true, false, false, "", "Il me manque une clé pour ouvrir ce cadenas", false);
+	blocked.init('battery', 'blocked.svg', 500, 0, 150, false, true, false,  "", "Il me manque une clé pour ouvrir ce cadenas", false);
 
 	var bookshelf = Object.create(Element);
-	bookshelf.init('bookshelf', 'img/bookshelf.svg', false, false, true, false, "", "oh, une carte Michelin!", false);
+	bookshelf.init('bookshelf', 'bookshelf.svg', 500, 0, 150, false, false, true,  "", "oh, une carte Michelin!", false);
 
 	var bowl = Object.create(Element);
-	bowl.init('bowl', 'img/bowl.svg', false, false, false, false, "", "Ceci est un bol", false);
+	bowl.init('bowl', 'bowl.svg', 500, 0, 150, false, false, false,  "", "Ceci est un bol", false);
 
 	var candle = Object.create(Element);
-	candle.init('candle', 'img/candle.svg', false, true, false, false, "", "Il me faut quelque chose pour allumer cette bougie", false);
+	candle.init('candle', 'candle.svg', 500, 0, 150, false, true, false,  "", "Il me faut quelque chose pour allumer cette bougie", false);
 
 	var candleOff = Object.create(Element);
-	candleOff.init('candleOff', 'img/candleOff.svg', false, false, false, false, "", "Quelque chose apparaît", false);
+	candleOff.init('candleOff', 'candleOff.svg', 500, 0, 150, false, false, false,  "", "Quelque chose apparaît", false);
 
 	var cheese = Object.create(Element);
-	cheese.init('cheese', 'img/cheese.svg', true, true, false, true, "", "A quoi ce fromage pourrait-il bien me servir?", false);
+	cheese.init('cheese', 'cheese.svg', 500, 0, 150, true, true, false,  "", "A quoi ce fromage pourrait-il bien me servir?", false);
 
 	var cigarette = Object.create(Element);
-	cigarette.init('cigarette', 'img/cigarette.svg', true, true, false, true, "", "Des cigarettes", false);
+	cigarette.init('cigarette', 'cigarette.svg', 500, 0, 150, true, true, false,  "", "Des cigarettes", false);
 
 	var coffeeMaker = Object.create(Element);
-	coffeeMaker.init('cigarette', 'img/coffee-maker.svg', false, false, false, false, "", "Une simple cafetière", false);
+	coffeeMaker.init('cigarette', 'coffee-maker.svg', 500, 0, 150, false, false, false,  "", "Une simple cafetière", false);
 
 	var compteur = Object.create(Element);
-	compteur.init('compteur', 'img/compteur.png', false, true, true, false, "", "Il manque quelque chose pour faire fonctionner l'ascenseur", false);
+	compteur.init('compteur', 'compteur.png', 500, 0, 150, false, true, true,  "", "Il manque quelque chose pour faire fonctionner l'ascenseur", false);
 
 	var compteurrempli = Object.create(Element);
-	compteurrempli.init('compteurrempli', 'img/compteurrempli.png', false, true, true, false, "", "Ca devrait fonctionner!", false);
+	compteurrempli.init('compteurrempli', 'compteurrempli.png', 500, 0, 150, false, true, true,  "", "Ca devrait fonctionner!", false);
 
 	var elevator = Object.create(Element);
-	elevator.init('elevator', 'img/elevator.svg', false, false, false, false, "", "Il manque quelque chose pour faire fonctionner l'ascenseur", false);
+	elevator.init('elevator', 'elevator.svg', 500, 0, 150, false, false, false,  "", "Il manque quelque chose pour faire fonctionner l'ascenseur", false);
 
 	var france = Object.create(Element);
-	france.init('france', 'img/france.svg', false, false, true, false, "", "Deux villes sont entourées sur la carte: Limoges et Bourges", false);
+	france.init('france', 'france.svg', 500, 0, 150, false, false, true,  "", "Deux villes sont entourées sur la carte: Limoges et Bourges", false);
 
 	var fridge = Object.create(Element);
-	fridge.init('fridge', 'img/fridge.svg', false, false, false, false, "", "Il y a un cadenas sur ce frigo", false);
+	fridge.init('fridge', 'fridge.svg', 500, 0, 150, false, false, false,  "", "Il y a un cadenas sur ce frigo", false);
 
 	var fuse = Object.create(Element);
-	fuse.init('fuse', 'img/fuse.svg', true, true, false, true, "", "A quoi ce fusile pourrait-il bien me servir?", false);
+	fuse.init('fuse', 'fuse.svg', 500, 0, 150, true, true, false,  "", "A quoi ce fusile pourrait-il bien me servir?", false);
 	
 	var gun = Object.create(Element);
-	gun.init('fuse', 'img/gun.svg', true, true, false, false, "", "Un pistolet", false);
+	gun.init('fuse', 'gun.svg', 500, 0, 150, true, true, false,  "", "Un pistolet", false);
 
 	// Objets de la 2ème pièce
 	var axe = Object.create(Element);
-	axe.init('axe', 'img/axe.svg', false, false, false, false, "", "Cette hache est verouillée sous un cadenas à 4 chiffres", false);
+	axe.init('axe', 'axe.svg', 500, 0, 150, false, false, false,  "", "Cette hache est verouillée sous un cadenas à 4 chiffres", false);
 	
 	var blueCircle = Object.create(Element);
-	blueCircle.init('blueCircle', 'img/blue-circle.svg', true, true, false, false, "", "?", false);
+	blueCircle.init('blueCircle', 'blue-circle.svg', 500, 0, 150, true, true, false,  "", "?", false);
 
 	var greenCircle = Object.create(Element);
-	greenCircle.init('greenCircle', 'img/green-circle.svg', true, true, false, false, "", "?", false);
+	greenCircle.init('greenCircle', 'green-circle.svg', 500, 0, 150, true, true, false,  "", "?", false);
 	
-	// function (name, urlImage, movable, combinable, zoomable, bagable, answer, message, inputable)
+	// function (name, urlImage, draggable, combinable, zoomable, answer, message, inputable)
 	var bourges = Object.create(Element);
-	bourges.init('bourges', 'img/bourges.png', false, false, true, false, "", "Ce tableau représente la cathédrale de Bourges", false);
+	bourges.init('bourges', 'bourges.png', 500, 0, 150, false, false, true,  "", "Ce tableau représente la cathédrale de Bourges", false);
 
 	var bourgesuv = Object.create(Element);
-	bourgesuv.init('bourgesuv', 'img/bourgesuv.png', false, false, true, false, "6", "Ce tableau représente la cathédrale de Bourges", false);
+	bourgesuv.init('bourgesuv', 'bourgesuv.png', 500, 0, 150, false, false, true,  "6", "Ce tableau représente la cathédrale de Bourges", false);
 	
 	var doorLocked = Object.create(Element);
-	doorLocked.init('doorLocked', 'img/door-locked.svg', false, true, false, false, "", "Cette porte est fermée, il faudrait quelque chose pour l’ouvrir", false);
+	doorLocked.init('doorLocked', 'door-locked.svg', 500, 0, 150, false, true, false,  "", "Cette porte est fermée, il faudrait quelque chose pour l’ouvrir", false);
 	
 	var doorOpen = Object.create(Element);
-	doorOpen.init('doorOpen', 'img/door-open.svg', false, false, false, false, "", "La porte est ouverte!!!", false);
+	doorOpen.init('doorOpen', 'door-open.svg', 500, 0, 150, false, false, false,  "", "La porte est ouverte!!!", false);
 	
 	var fireAlarm = Object.create(Element);
-	fireAlarm.init('fireAlarm', 'img/fire-alarm.svg', false, false, false, false, "", "Je ne peux pas passer, une alarme a été activée", false);
+	fireAlarm.init('fireAlarm', 'fire-alarm.svg', 500, 0, 150, false, false, false,  "", "Je ne peux pas passer, une alarme a été activée", false);
 		
 	var flashlight = Object.create(Element);
-	flashlight.init('flashlight', 'img/flashlight.svg', false, false, false, false, "", "On peut désactiver cette alarme grâce à un code couleur", true);
+	flashlight.init('flashlight', 'flashlight.svg', 500, 0, 150, false, false, false,  "", "On peut désactiver cette alarme grâce à un code couleur", true);
 		
 } ); // fermeture JQuery
 
